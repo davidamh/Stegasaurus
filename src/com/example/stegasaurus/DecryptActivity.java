8c74a8a7ed62dcb412487b6ea2949ab0307f55f4
David Hernandez <davidamh@vt.edu>
2013-12-05 11:07:56 -0500

    Made layout for decrypt screen.

--- src/com/example/stegasaurus/DecryptActivity.java
+++ src/com/example/stegasaurus/DecryptActivity.java
@@ -1,13 +1,22 @@
 package com.example.stegasaurus;
 
+import java.io.InputStream;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class DecryptActivity extends Activity {
+	
+	final int REQUEST_CHOOSE_IMAGE = 7384;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +24,10 @@ public class DecryptActivity extends Activity {
 		setContentView(R.layout.activity_decrypt);
 		// Show the Up button in the action bar.
 		setupActionBar();
+		
+		Intent choosePictureIntent = new Intent(Intent.ACTION_PICK);
+		choosePictureIntent.setType("image/*");
+		startActivityForResult(choosePictureIntent, REQUEST_CHOOSE_IMAGE);
 	}
 
 	/**
@@ -48,6 +61,30 @@ public class DecryptActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	public void returnToMain(View view) {
+		finish();
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) { 
+		if(requestCode == REQUEST_CHOOSE_IMAGE && resultCode == RESULT_OK){  
+			try {
+	            Uri selectedImage = imageReturnedIntent.getData();
+	            InputStream imageStream = getContentResolver().openInputStream(selectedImage);
+	            Bitmap pic = BitmapFactory.decodeStream(imageStream);
+	            String msg = decrypt(pic);
+	            
+	            if(msg != null) {
+		            TextView decMsg = (TextView) findViewById(R.id.dec_msg_view);
+		            decMsg.setText(msg);
+	            }
+			}
+			catch(Exception e) {
+				e.printStackTrace();
+			}
+        }
 	}
 
 	private String decrypt(Bitmap pic) {
